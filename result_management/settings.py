f91c13d1758760dce321bb3c9dba1370d7a3f3cc
Vedant Shimpi <vedshimpi77@gmail.com>
2026-02-26 10:44:24 +0530

    Removed pycache and updated gitignore

--- result_management/settings.py
+++ result_management/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-result-management-system-saas-b2b-2024-change-in-production'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -27,7 +27,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'result_management.urls'
